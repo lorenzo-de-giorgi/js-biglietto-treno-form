bc018328c0c1156db512125b6f9e959574795cc6
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-07 14:19:13 +0100

    doing js file

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//calcoli perbiglietti
+let normalPrice = (km * 0.21).toFixed(2);
+let underagePrice = (20 / 100);
+let overPrice = (40 / 100);
+
+let createTicket = document.getElementById('create-ticket');
+createTicket.addEventListener('click', function(){
+    
+    }
+)
