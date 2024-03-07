b3878da99806bd1cf381b94def40325c065301d0
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-07 17:19:39 +0100

    exercise completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,59 @@
-//calcoli perbiglietti
-let normalPrice = (km * 0.21).toFixed(2);
-let underagePrice = (20 / 100);
-let overPrice = (40 / 100);
+//recupero dati
+let nameSurname = document.getElementById('name');
+let distance = document.getElementById('distance');
+let age = document.getElementById('age');
 
+//dati table
+let elName = document.getElementById('elName');
+let elOfferta = document.getElementById('elOfferta');
+let elCarrozza = document.getElementById('elCarrozza');
+let elCode = document.getElementById('elCode');
+let elCost = document.getElementById('elCost');
+
+// invia button
 let createTicket = document.getElementById('create-ticket');
 createTicket.addEventListener('click', function(){
-    
-    }
-)
+    const ticketName = nameSurname.value;
+    const travelDistance = distance.value;
+    const ageTraveller = age.value;
+    //calcoli perbiglietti
+    let normalPrice = (travelDistance * 0.21);
+    let underagePrice = (normalPrice - (normalPrice * (20 / 100)));
+    let overPrice = (normalPrice - (normalPrice * (40 / 100)));
+    console.log(ticketName, travelDistance, ageTraveller);
+    let price;
+    let offerta;
+
+    if(ageTraveller == 'underage'){
+        price = underagePrice
+        offerta = '-20% Minorenne'
+    } else if(ageTraveller == 'over65'){
+        price = overPrice
+        offerta = '-40% Over 65'
+    } else {
+        price = normalPrice
+        offerta = 'Nessun offerta'
+    };
+
+    let carrozza = getRndInteger(1, 12);
+    let code = getRndInteger(5000, 10000)
+
+    //stampo dati in table
+    elName.innerText = ticketName;
+    elOfferta.innerText = offerta;
+    elCarrozza.innerText = carrozza;
+    elCode.innerText = code;
+    elCost.innerText = price.toFixed(2);
+});
+
+// annulla button
+let cancelTicket = document.getElementById('reset');
+cancelTicket.addEventListener('click', function() {
+    document.getElementById('name').value = '';
+    document.getElementById('distance').value = '';
+    elName.innerText = '';
+    elOfferta.innerText = '';
+    elCarrozza.innerText = '';
+    elCode.innerText = '';
+    elCost.innerText = '';
+});
